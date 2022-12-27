0f16ec92588479aa91cd01a75838f0a43fbbfe35
Mohi <111868991+mohifk@users.noreply.github.com>
2022-12-27 19:17:18 +0330

    create apps (blog and accounts) in docker

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'blog',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
