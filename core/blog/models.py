d5d3ca82a5b20b731221d8a4ba1087558ccc5324
Mohi <111868991+mohifk@users.noreply.github.com>
2022-12-27 22:38:16 +0330

    Update models.py

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+class Post(models.Model):
+    ''' this is a class to define posts for blog '''
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
+    image = models.ImageField(null=True,blank=True)
+    title= models.CharField(max_length=250)
+    content=models.TextField()
+    status = models.BooleanField()
+    category=models.ForeignKey('category',on_delete=models.SET_NULL,null=True)
 
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now=True)
+    published_date=models.DateTimeField()
+
+    def __str__(self) :
+        return self.title
+
+class category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
