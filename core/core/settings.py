6a3bba792dda0846b38375f72033caadb9651d62
Mohi <111868991+mohifk@users.noreply.github.com>
2022-12-27 13:41:05 +0330

    create docker compose

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g8vgqa@j_!vuwf@dhq3a&b#p8&21ajnzwko61*xzxv+)e_o%fv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')],default="*")
 
 
 # Application definition
--- dockerfile
+++ dockerfile
@@ -7,5 +7,5 @@ RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
 
 COPY ./core /app/
-CMD ["python3","manage.py","runserver","0.0.0.0:8000"]
+# CMD ["python3","manage.py","runserver","0.0.0.0:8000"]
 
