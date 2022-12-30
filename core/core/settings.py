da2fa2e7861ac84b872b0e051d0680b2b3771a01
Mohi <111868991+mohifk@users.noreply.github.com>
2022-12-30 22:15:19 +0330

    create model user account
    create  base settings  and models of user and super user and manager of them

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
+    #'blog',
 ]
 
 
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#user manager config
+AUTH_USER_MODEL = 'accounts.User'
