e456cc7a85c4a5a72425270114369f2326fb6f04
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-06 21:13:53 +0330

    introduction Template view
    how to work template view in views.py

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,7 +1,8 @@
-from .views import indexView
+from . import views
 from django.urls import path,include
 from django.views.generic import TemplateView
 urlpatterns = [
-    path('fbv-index',indexView,name='fbv-index'),
-    path('cbv-index',TemplateView.as_view(template_name='index.html',extra_context={'name':'mohi'})),
+    path('fbv-index',views.indexView,name='fbv-index'),
+    #path('cbv-index',TemplateView.as_view(template_name='index.html',extra_context={'name':'mohi'})),
+    path('cbv-index',views.IndexView.as_view(),name='cbv-index')
     ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Post
+
 def indexView(request):
     name='ali'
     context={'name':name}
     return render(request,"index.html",context)
 # Create your views here.
+class IndexView(TemplateView):
+    template_name ='index.html'
+
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["name"]="mohi"
+        context["posts"]=Post.objects.all()
+        return context
