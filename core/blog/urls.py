0decadee852a849f7b4f048ffb79028ad92ffe46
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-06 22:01:44 +0330

    RedirectView introduction

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path,include
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+app_name="blog"
 urlpatterns = [
     path('fbv-index',views.indexView,name='fbv-index'),
     #path('cbv-index',TemplateView.as_view(template_name='index.html',extra_context={'name':'mohi'})),
-    path('cbv-index',views.IndexView.as_view(),name='cbv-index')
+    path('cbv-index',views.IndexView.as_view(),name='cbv-index'),
+    path('goto_googl',RedirectView.as_view(url='https://digikala.com/'),name='google'),
+    path('go_to_index',RedirectView.as_view(pattern_name="blog:cbv-index"),name='redirect_to_index'),
+    
     ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -3,11 +3,16 @@ from django.views.generic.base import TemplateView
 from .models import Post
 
 def indexView(request):
+
+    '''a function based view to show index page'''
+
     name='ali'
     context={'name':name}
     return render(request,"index.html",context)
 # Create your views here.
 class IndexView(TemplateView):
+    '''a class based view to show index page'''
+
     template_name ='index.html'
 
     def get_context_data(self, **kwargs):
