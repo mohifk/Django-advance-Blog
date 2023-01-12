b07203f04ea7b0a9be90807a2cf072d7d3adef17
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-12 20:07:04 +0330

    deleteView
    delete post with button and url

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('post/',views.PostListView.as_view(),name='post_list'),
     path('post/<int:pk>/',views.PostDetailView.as_view(),name="post-detail"),
     path('post/create/',views.PostCreateView.as_view(),name="post-create"),
-    path('post/<int:pk>/edit/',views.PostEditView.as_view(),name="post-edit")
+    path('post/<int:pk>/edit/',views.PostEditView.as_view(),name="post-edit"),
+    path('post/<int:pk>/delete/',views.PostDeleteView.as_view(),name="post-delete")
     ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView,RedirectView
 from django.shortcuts import redirect,get_object_or_404
 from .models import Post
-from django.views.generic import ListView , DetailView ,FormView,CreateView,UpdateView
+from django.views.generic import ListView , DetailView ,FormView,CreateView,UpdateView,DeleteView
 from.forms import PostForm
 def indexView(request):
 
@@ -68,4 +68,8 @@ class PostCreateView(CreateView):
 class PostEditView(UpdateView):
     model=Post
     form_class=PostForm
+    success_url='/blog/post/'
+
+class PostDeleteView(DeleteView):
+    model=Post
     success_url='/blog/post/'
