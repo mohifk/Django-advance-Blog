57f9d5ee1c33ef6e2786a1abc6f4920be99885a3
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-19 22:41:01 +0330

    view list post with serializer api

--- core/blog/api/V1/views.py
+++ core/blog/api/V1/views.py
@@ -6,10 +6,13 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
-    return Response("ok")
+    posts=Post.objects.filter(status=True)
+    serializer=PostSerialilzer(posts,many=True)
+    return Response(serializer.data)
 
 @api_view()
 def postDetail(request,id):
     post=get_object_or_404(Post,pk=id)
     serializer=PostSerialilzer(post)
     return Response(serializer.data)
+ 
