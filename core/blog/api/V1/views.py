d71385cfa23c26002dad98eccc2e29c303e9a4dc
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-19 21:00:12 +0330

    if post not exist print not found with rest

--- core/blog/api/V1/views.py
+++ core/blog/api/V1/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerialilzer
 from ...models import Post
-
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def postList(request):
@@ -10,6 +10,6 @@ def postList(request):
 
 @api_view()
 def postDetail(request,id):
-    post=Post.objects.get(pk=id)
+    post=get_object_or_404(Post,pk=id)
     serializer=PostSerialilzer(post)
     return Response(serializer.data)
