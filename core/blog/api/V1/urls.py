43415d3e4de76b01478938aa668e7530fb5638e4
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-27 17:22:56 +0330

    create automatic urls with defult ruter

--- core/blog/api/V1/urls.py
+++ core/blog/api/V1/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name = "api_v1"
 
-urlpatterns=[
+router =DefaultRouter()
+router.register('post',views.PostViewSet,basename='post')
+
+urlpatterns=router.urls
+# urlpatterns=[
     # path('post/',views.postList,name='post-list'),
     # # path('post/<int:id>/',views.postDetail,name="post-detail"),
     # path('post/',views.PostList.as_view(),name='post-list'),
     # path('post/<int:pk>/',views.PostDetail.as_view(),name="post-detail"),
-    path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
-    path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name="post-detail"),
+    # path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
+    # path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name="post-detail"),
 
-    ]
+    # ] 
