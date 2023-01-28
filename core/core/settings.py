f4f625b6eb66a0f5366e51090d45e148ca8bccc7
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-28 22:45:39 +0330

    create api document with core api and docs url

--- core/core/settings.py
+++ core/core/settings.py
@@ -136,8 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #user manager config
 AUTH_USER_MODEL = 'accounts.User'
 
-REST_FRAMEWORK = {
+#REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': [
 #         'rest_framework.permissions.IsAuthenticated',
 #     ]
- }
+# }
+
+
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
