74d601e9a0da7768c1f1c1757228e9a11c33d8f5
Mohi <111868991+mohifk@users.noreply.github.com>
2023-01-30 22:31:53 +0330

    use the serializer for create field to get complatly of urls post page

--- core/blog/api/V1/serializers.py
+++ core/blog/api/V1/serializers.py
@@ -8,11 +8,14 @@ from ...models import Post,category
 class PostSerialilzer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
-    
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+
     class Meta:
         model=Post
-        fields=['id','title','author','content','snippet','category','status','relative_url','created_date','published_date']
-
+        fields=['id','title','author','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+    def get_abs_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj)
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=category
