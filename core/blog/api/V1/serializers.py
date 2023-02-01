b34e3c7a88fd148f864748129d509667da9f4c60
Mohi <111868991+mohifk@users.noreply.github.com>
2023-02-01 22:42:11 +0330

    use authenticated the user for create post

--- core/blog/api/V1/serializers.py
+++ core/blog/api/V1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from ...models import Post,category
+from accounts.models import Profile
 
 # class PostSerialilzer(serializers.Serializer):
     # id=serializers.IntegerField()
@@ -17,7 +18,8 @@ class PostSerialilzer(serializers.ModelSerializer):
     #category=serializers.SlugRelatedField(many=False,slug_field='name',queryset=category.objects.all())
     class Meta:
         model=Post
-        fields=['id','title','author','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+        fields=['id','title','author','image','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
+        read_only_fields=['author']
     def get_abs_url(self,obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj)
@@ -29,6 +31,10 @@ class PostSerialilzer(serializers.ModelSerializer):
             rep.pop('snippet')
             rep.pop('relative_url')
             rep.pop('absolute_url')
-        rep['category']=CategorySerializer(instance.category).data
+        rep['category']=CategorySerializer(instance.category,context={'request':request}).data
         
         return rep
+
+    def create(self, validated_data):
+        validated_data['author']=Profile.objects.get(user__id = self.context.get('request').user.id)
+        return super().create(validated_data)
