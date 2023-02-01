19cbb0e1da5733b5d7168e5685f44d087f87bb61
Mohi <111868991+mohifk@users.noreply.github.com>
2023-02-01 19:55:22 +0330

    Update serializers.py

--- core/blog/api/V1/serializers.py
+++ core/blog/api/V1/serializers.py
@@ -5,18 +5,19 @@ from ...models import Post,category
     # id=serializers.IntegerField()
     # title=serializers.CharField(max_length=255)
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=category
+        fields=['id','name']   
+
 class PostSerialilzer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
     absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
-    category=serializers.SlugRelatedField(many=False,slug_field='name',queryset=category.objects.all())
+    #category=serializers.SlugRelatedField(many=False,slug_field='name',queryset=category.objects.all())
     class Meta:
         model=Post
         fields=['id','title','author','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']
     def get_abs_url(self,obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj)
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model=category
-        fields=['id','name']   
