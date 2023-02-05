c0ea81f8f3963d3f5b1c3107c0755482d2e7193f
Mohi <111868991+mohifk@users.noreply.github.com>
2023-02-05 22:47:19 +0330

    create api accounts

--- core/core/urls.py
+++ core/core/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls')),
-    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('api-docs',include_docs_urls(title='api sample')),
     path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
