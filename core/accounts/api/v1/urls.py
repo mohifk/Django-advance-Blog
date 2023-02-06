fdb0328a28321246e7b8a5be4805ca12ddb313d4
Mohi <111868991+mohifk@users.noreply.github.com>
2023-02-06 23:04:02 +0330

    create accounts api model

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
+from .import views
 
-
+app_name='api-v1'
 
 urlpatterns = [
-
-    ]
+#registration
+path('registration/',views.RegistrationApiView.as_view(),name='registration')
+    ] 
  
---
+++ core/accounts/api/v1/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegistrationSerializer
+
+class RegistrationApiView(generics.GenericAPIView):
+    serializer_class = RegistrationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = RegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'email':serializer.validated_data['email']
+            }
+            return Response(data,status=status.HTTP_201_CREATED)
