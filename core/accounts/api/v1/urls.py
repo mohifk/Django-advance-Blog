97951f45eff7742b27e3005eba3fdeef1f9a0a5d
Mohi <111868991+mohifk@users.noreply.github.com>
2023-02-14 21:37:13 +0330

    create custom token authentication(response)
    now when in swagger mod
    add accounts../token/login
    you can login with email and password
    and show to you if login email and token and id

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
 from .import views
-from rest_framework.authtoken.views import ObtainAuthToken
+#from rest_framework.authtoken.views import ObtainAuthToken
 app_name='api-v1'
 
 urlpatterns = [
 #registration
 path('registration/',views.RegistrationApiView.as_view(),name='registration'),
-path('token/login',ObtainAuthToken.as_view(),name='token-login')
+path('token/login/',views.CustomObtainAuthToken.as_view(),name='token-login')
     ] 
  
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer,CustomAuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
@@ -15,3 +17,16 @@ class RegistrationApiView(generics.GenericAPIView):
             }
             return Response(data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomObtainAuthToken(ObtainAuthToken):
+    serializer_class=CustomAuthTokenSerializer
+    def post(self,request,*args,**kwargs):
+        serializer=self.serializer_class(data=request.data,context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user=serializer.validated_data['user']
+        token,created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token':token.key,
+            'user.id':user.pk,
+            'email':user.email
+        })
