dc53112681bf65b7f07d88159c0254efd2b2e8f5
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-10 09:17:01 +0200

    update blog urls for index in blog

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # path('goto_googl',RedirectView.as_view(url='https://digikala.com/'),name='google'),
     # path('go_to_index',RedirectView.as_view(pattern_name="blog:cbv-index"),name='redirect_to_index'),
     # path('go-to-maktab/<int:pk>/',views.Redirecttomaktab.as_view(),name='redirect-to-maktab'),
+    path("",views.IndexView.as_view(),name="index"),
     path("post/", views.PostListView.as_view(), name="post_list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
     path("post/create/", views.PostCreateView.as_view(), name="post-create"),
