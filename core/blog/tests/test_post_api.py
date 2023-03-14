98951300e70279651cb937c952a529a911b84ca5
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-14 23:07:12 +0330

    Update test_post_api.py

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -26,16 +26,15 @@ class TestPostApi:
         response= api_client.get(url)
         assert response.status_code==200
     
-    def test_create_post_respnse_401_status(self,api_client,common_user):
+    def test_create_post_respnse_403_status(self,api_client):
         url=reverse("blog:api_v1:post-list")
         data={"title":"test",
               "content":"description",
               "status":True,
               "published_date":datetime.now() }
-        user=common_user
-        api_client.force_authenticate(user=user)
+     
         response= api_client.post(url,data)
-        assert response.status_code==201
+        assert response.status_code==403
     
     def test_create_post_response_201_status(self,api_client,common_user):
         url=reverse("blog:api_v1:post-list")
