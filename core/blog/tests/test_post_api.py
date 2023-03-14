5bc60778a11dd55027788969e8cfe61a6f4d52f8
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-14 23:04:38 +0330

    create py test test response and create post and invalid data

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -2,20 +2,62 @@ import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
 from datetime import datetime
+from accounts.models import User
+
+@pytest.fixture
+def api_client():
+    client=APIClient()
+    return client
+@pytest.fixture
+def common_user():
+    user=User.objects.create_user(email="t2@admin.com",password="123")
+    return user
+
+
+
 
 @pytest.mark.django_db
 class TestPostApi:
-    client=APIClient()
-    def test_get_post_response_200_status(self):
-        url=reverse("blog:api_v1:post-list")
-        response= self.client.get(url)
+    #client=APIClient()
+    def test_get_post_response_200_status(self,api_client,common_user):
+        user=common_user
+        api_client.force_authenticate(user=user)
+        url =reverse("blog:api_v1:post-list")
+        response= api_client.get(url)
         assert response.status_code==200
-
-    def test_create_post_respnse_401_status(self):
+    
+    def test_create_post_respnse_401_status(self,api_client,common_user):
         url=reverse("blog:api_v1:post-list")
         data={"title":"test",
               "content":"description",
               "status":True,
               "published_date":datetime.now() }
-        response=self.client.post(url,data)
-        assert response.status_code==407
+        user=common_user
+        api_client.force_authenticate(user=user)
+        response= api_client.post(url,data)
+        assert response.status_code==201
+    
+    def test_create_post_response_201_status(self,api_client,common_user):
+        url=reverse("blog:api_v1:post-list")
+        data={"title":"test",
+              "content":"description",
+              "status":True,
+              "published_date":datetime.now() }
+        user=common_user
+        api_client.force_authenticate(user=user)
+        response=api_client.post(url,data)
+        assert response.status_code == 201
+
+    def test_create_post_invalid_data_response_400_status(self,api_client,common_user):
+        url=reverse("blog:api_v1:post-list")
+        data={"title":"test",
+              "content":"description",
+         }
+        user=common_user
+        api_client.force_authenticate(user=user)
+        response=api_client.post(url,data)
+        assert response.status_code == 400
+
+
+
+ 
