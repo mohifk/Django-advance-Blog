37154e7caa808c5478334845c4d8bf37844024f0
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-15 21:59:15 +0330

    Update insert_data.py

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -2,8 +2,9 @@ from django.core.management.base import BaseCommand
 from faker import Faker
 from accounts.models import User,Profile
 from blog.models import Post,category
-
-
+import random
+from datetime import datetime
+category_list=["it","life","design","fun","sport","camera"]
 class Command(BaseCommand):
     help="inserting dummy data"
     def __init__(self, *args,**kwargs):
@@ -16,3 +17,14 @@ class Command(BaseCommand):
         profile.last_name=self.fake.last_name()
         profile.description=self.fake.paragraph(nb_sentences=5)
         profile.save()
+        for name in category_list:
+            category.objects.get_or_create(name=name)
+        for _ in range(10):
+            Post.objects.create(author=profile,
+                                title=self.fake.paragraph(nb_sentences=1),
+                                content=self.fake.paragraph(nb_sentences=15),
+                                status=random.choice([True,False]),
+                                category=category.objects.get(name=random.choice(category_list)),
+                                published_date= datetime.now()
+                               
+                                )
