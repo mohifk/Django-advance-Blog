385e883ddccd0c9b5449f6e1cfa4c58d243052fb
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-16 17:16:02 +0200

    create load by locustfile and locust

---
+++ core/locust/locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpUser,task,between
+class QuickStartUser(HttpUser):
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
+    @task
+    def post_category(self):
+        self.client.get("/blog/api/v1/category/")
+
+    def on_start(self):
+        respnse=self.client.post('/accounts/api/v2/jwt/create/',data={'email':'admin@admin.com',"password":"123"}).json()
+        self.client.headers={'Authorization':f"Bearer{respnse.get('access',None)}"}
+        return super().on_start()
