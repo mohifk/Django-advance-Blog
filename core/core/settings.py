c217f76822e9fd2b383c7650bcfcecdd541c9f57
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-16 16:01:27 +0200

    cors header for api access for out of local

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
+    "corsheaders",
     "accounts",
     "blog",
     "rest_framework",
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -176,3 +178,8 @@ EMAIL_PORT = 25
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://127.0.0.1:5300",
+]
