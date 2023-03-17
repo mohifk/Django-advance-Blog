2dcd2e5234e6ac5fd19f7f76e7994c21f767c628
Mohi <111868991+mohifk@users.noreply.github.com>
2023-03-17 08:30:36 +0200

    some solve some bugs

--- core/accounts/task.py
+++ core/accounts/task.py
@@ -2,6 +2,6 @@ from celery import shared_task
 from time import sleep
 
 @shared_task
-def SendEmail()
+def SendEmail():
     sleep(3)
     print('done send email')
--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-
+from . import views
 app_name = "accounts"
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("api/v1/", include("accounts.api.v1.urls")),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
+    path('send_email',views.send_email,name='send-email'),
 ]
